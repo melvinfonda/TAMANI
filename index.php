84f35e542c43f9d3fea786b4a3ea95cce9f18818
susantigojali <susantigojali@hotmail.com>
2015-02-06 14:53:50 +0700

    ganti pengaduan

--- index.php
+++ index.php
@@ -30,11 +30,14 @@ require_once('login.php'); // Includes Login Script
         <div class="col-sm-12 col-md-12">
           <div class="thumbnail">
             <div class="caption">
-                <dl>
-                  <p> 23-Januari-2015  | Fonda | belum diproses </p>
+              <dl>
+                <p> 23-Januari-2015  | Fonda | belum diproses </p>
 
-                </dl>
-              <h5><b>Taman Pasupati</b></h5>
+              </dl>
+              <h3> Pengaduan apa gitu</h3>
+              <h5> 
+                <b>Taman Pasupati</b>
+              </h5>
               <p>saya mendaftar secara online bpjs kesehatan pada tanggal 4-Feb-2015 dengan nomor registrasi pendaftaran 1[***************]0[***************][***************]1614 an. MULIANI & DESIKA. 
                 setelah mendapatkan email konfirmasi dari admin BPJS dan mendownload virtual account, saya melakukan pembayaran via atm BRI. setelah pembayaran sukses, saya coba buka kembali link yg dikirim oleh admin BPJS via email untuk mencetak e-id. namun link untuk mencetak e-id tidak muncul dan ada keterangan "belum membayar iuran". 
                 saya sudah coba berkali kali namun keterangan tetap sama. mohon tindak lanjutnya agar saya dapat mencetak e-id tersebut secara online. </p>
